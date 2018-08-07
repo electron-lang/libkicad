a7a00fa92e84add84e7aa2bbebe62271177f9cd0
David Craven <david@craven.ch>
2018-08-07 15:25:54 +0200

    Remove console.log stmt.

--- tests/tinyfpga.ts
+++ tests/tinyfpga.ts
@@ -2,13 +2,11 @@ import { expect } from 'chai'
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
 import { netlist } from '../src'
-import { tokenize } from '../src/netlist/parser'
 
 describe('tinyfpga', () => {
     it('should parse', () => {
         const p = resolve('./tests/TinyFPGA-B.net')
         const t = readFileSync(p)
-        console.log(tokenize(t.toString()))
         const d = netlist.parse(t.toString())
         expect(d.tool).to.equal("Eeschema 4.0.7")
     })
