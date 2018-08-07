b0a1fc09d09daf8616ee148b786a93ea9f8fce57
David Craven <david@craven.ch>
2018-08-07 18:07:50 +0200

    Improve error messages for equivalence checker.

--- src/netlist/equiv.ts
+++ src/netlist/equiv.ts
@@ -1,30 +1,75 @@
 import { Design, Component, Net, Node } from './ast'
-//import { Printer } from './printer'
+import { printerInstance } from './printer'
 
 export class Equiv {
     constructor(protected design1: Design, protected design2: Design) {}
 
-    equiv(): boolean {
-        if (this.design1.nets.length !== this.design2.nets.length) {
-            return false
-        }
-        const otherNets = this.design2.nets.map((net) => net)
-        for (let net1 of this.design1.nets) {
+    protected _equiv<T>(list1: T[], list2: T[], equiv: (a: T, b: T) => boolean): [T[], T[]] {
+        // create a copy
+        list2 = list2.map((x) => x)
+        const noMatch: T[] = []
+        for (let a of list1) {
             let i = 0
-            for (let net2 of otherNets) {
-                if (this.equivNet(net1, net2)) {
+            for (let b of list2) {
+                if (equiv(a, b)) {
                     break
                 }
                 i += 1
             }
-            if (i < otherNets.length) {
-                otherNets.splice(i, 1)
+            if (i < list2.length) {
+                list2.splice(i, 1)
             } else {
-                console.log(`No matching net for ${net1.name}`)
-                return false
+                noMatch.push(a)
             }
         }
-        return true
+        return [noMatch, list2]
+    }
+
+    equiv(): boolean {
+        if (this.equivComponents()) {
+            return this.equivNets()
+        }
+        return false
+    }
+
+    equivComponents(): boolean {
+        const [nomatch, leftover] = this._equiv(this.design1.components,
+                                                this.design2.components,
+                                                this.equivComponent)
+        for (let a of nomatch) {
+            console.log(`Component ${printerInstance.render(a)} not found.`)
+        }
+
+        for (let b of leftover) {
+            console.log(`Component ${printerInstance.render(b)} leftover.`)
+        }
+
+        return nomatch.length == 0 && leftover.length == 0
+    }
+
+    equivNets(): boolean {
+        const [nomatch, leftover] = this._equiv(this.design1.nets,
+                                                this.design2.nets,
+                                                (a, b) => this.equivNet(a, b))
+
+        if (nomatch.length > 0) {
+            console.log('Unmatched nets:')
+            for (let a of nomatch) {
+                console.log(a.nodes.map((n) => n.unwrap()))
+            }
+        }
+
+        if (leftover.length > 0) {
+            console.log('Leftover nets:')
+            for (let b of leftover) {
+                console.log(b.nodes.map((n) => n.unwrap()))
+            }
+        }
+
+        const nmatches = this.design1.nets.length - nomatch.length
+        console.log(`Matched ${nmatches} of ${this.design1.nets.length} nets.`)
+
+        return nomatch.length == 0 && leftover.length == 0
     }
 
     equivNet(net1: Net, net2: Net): boolean {
